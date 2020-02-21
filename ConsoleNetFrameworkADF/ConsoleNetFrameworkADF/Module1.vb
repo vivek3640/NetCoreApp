5f3d15b1901c357c9bb1f0c97397fe9826100076
Vivek Kumar <vivek.kumar1@espire.com>
2020-02-21 10:55:19 +0530

    New work

---
+++ ConsoleNetFrameworkADF/ConsoleNetFrameworkADF/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("Executed console application")
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ ConsoleNetFrameworkADF/ConsoleNetFrameworkADF/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleNetFrameworkADF")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleNetFrameworkADF")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4d717922-ebf5-4c36-a735-7629bb3be234")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
